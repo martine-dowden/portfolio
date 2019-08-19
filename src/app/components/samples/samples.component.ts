c53299f51ff027b582973dd5a253a9ec2283e016
Martine Dowden <martine@dowden.us>
2019-08-19 02:49:10 -0400

    update cirrostyle info

--- src/app/components/samples/samples.component.ts
+++ src/app/components/samples/samples.component.ts
@@ -14,46 +14,18 @@ export class SamplesComponent implements OnInit {
       icon: 'assets/img/flexepark_car.svg',
       description: 'A friendly way to park',
       links: [
+        { href: 'https://about.flexepark.com/home', text: 'About' },
         { href: 'https://flexepark.com', text: 'App' }
-      ]
-    },
-    {
-      flipped: false,
-      name: 'Librum Arcanum',
-      icon: 'assets/img/librum_arcanum.svg',
-      description: 'The Pathfinder Spellbook',
-      links: [
-        { href: 'https://www.librumarcanum.com/', text: 'Site' },
-        { href: 'https://app.librumarcanum.com/', text: 'App' }
       ]
     },
     {
       flipped: false,
       name: 'cirrostyle',
       icon: 'assets/img/cirrostyle_icon.svg',
-      description: 'Easily manage your vector images, converting them to custom-sized raster files on demand.',
+      description: 'Organize, Manage, & Share Your Brand',
       links: [
+        { href: 'https://cirrostyle.com', text: 'About' },
         { href: 'https://app.cirrostyle.com', text: 'App' },
-      ]
-    },
-    {
-      flipped: false,
-      name: '.css {display: what?}',
-      icon: 'assets/img/css.svg',
-      description: 'Demo of the different values for the css display property',
-      links: [
-        { href: 'https://github.com/martine-dowden/display-what', text: 'Repo' },
-        { href: 'https://martine-dowden.github.io/display-what/index.html', text: 'Site' }
-      ]
-    },
-    {
-      flipped: false,
-      name: 'Playing With Fire',
-      icon: 'assets/img/fireside.svg',
-      description: 'Firebase Workshop',
-      links: [
-        { href: 'http://playingwithfire.live/', text: 'Site' },
-        { href: 'https://github.com/Mx3-Studio', text: 'Repo' }
       ]
     },
     {
@@ -67,6 +39,37 @@ export class SamplesComponent implements OnInit {
         { routerLink: ['/presentation/accessibility-cookbook'], text: 'Slides'}
       ]
     },
+    {
+      flipped: false,
+      name: 'Librum Arcanum',
+      icon: 'assets/img/librum_arcanum.svg',
+      description: 'The Pathfinder Spellbook',
+      links: [
+        { href: 'https://www.librumarcanum.com/', text: 'About' },
+        { href: 'https://app.librumarcanum.com/', text: 'App' }
+      ]
+    },
+    {
+      flipped: false,
+      name: '.css {display: what?}',
+      icon: 'assets/img/css.svg',
+      description: 'Demo of the different values for the css display property',
+      links: [
+        { href: 'https://github.com/martine-dowden/display-what', text: 'Repo' },
+        { href: 'https://martine-dowden.github.io/display-what/index.html', text: 'Demo' }
+      ]
+    },
+    {
+      flipped: false,
+      name: 'Playing With Fire',
+      icon: 'assets/img/fireside.svg',
+      description: 'Firebase Workshop',
+      links: [
+        { href: 'http://playingwithfire.live/', text: 'Site' },
+        { href: 'https://github.com/Mx3-Studio', text: 'Repo' }
+      ]
+    },
+
   ];
 
   constructor() { }
